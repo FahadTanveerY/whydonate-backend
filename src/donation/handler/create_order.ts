a0e618c95e60029025275cd4fdf3fcbbeb6d9dde
FahadTanveerY <fahad@whydonate.nl>
2022-06-15 01:17:44 +0500

    create order with mollie

--- src/donation/handler/create_order.ts
+++ src/donation/handler/create_order.ts
@@ -25,11 +25,27 @@ try {
     // })).json();
     // Store data in donation_order table
 
-    const res=await fetch(`https://whydonate_backend.whydonate.workers.dev/mollie/getpayment/tr_Pd6prr4ZVC`,{
+    // const res = await (await fetch(`https://api.mollie.com/v2/payments/tr_Pd6prr4ZVC`,{
+    //     method: 'GET',
+    //     headers:{
+    //         Authorization: 'Bearer test_jtggpgvcAGTufGj96j6WBSBVPJKrw8'
+    //     }
+    // })).json()
+    const res = await (await fetch(`https://api.mollie.com/v2/payments`, {
+        method: 'POST',
         headers: {
-                     'Content-Type': 'application/json',
-               },
-    })
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer test_jtggpgvcAGTufGj96j6WBSBVPJKrw8'
+        },
+        body: JSON.stringify({
+            amount:body.amount,
+            description: body.description,
+            webhookUrl: "https://webshop.example.org/order/12345/",
+            redirectUrl: "https://webshop.example.org/payments/webhook/",
+            metadata: body.metadata,
+            locale: body.locale
+        })
+    })).json()
     return new Response(JSON.stringify({
         res
     }),{
@@ -38,7 +54,7 @@ try {
 } catch (error) {
     return new Response(
         JSON.stringify({
-            status: 'failed',
+            status: 'failed 45',
             handler: 'donation/handler/create_order',
             time: new Date(),
             error: error
